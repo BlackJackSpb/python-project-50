878c38a24b017da5528b0df8f2249034d2cf2a56
Rystzov <rystzov1995@yandex.ru>
2025-01-22 11:41:13 +0300

    test

--- tests/test_jaml.py
+++ tests/test_jaml.py
@@ -6,3 +6,11 @@ file2 = 'tests/fixtures/file2.yml'
 
 def test_file1_vs_file2():
     assert generate_diff(file1, file2) == open('tests/fixtures/result_f1_f2_json.txt').read().strip()
+
+
+def test_file1_vs_file1():
+    assert generate_diff(file1, file1) == open('tests/fixtures/file1.txt').read().strip()
+
+
+def test_file2_vs_file2():
+    assert generate_diff(file2, file2) == open('tests/fixtures/file2.txt').read().strip()
