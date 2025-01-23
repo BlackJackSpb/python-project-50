274b2c4733b2397110d6b60caddd8c39453b31de
Rystzov <rystzov1995@yandex.ru>
2025-01-23 10:08:12 +0300

    fix v1.1

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,22 +2,28 @@ from gendiff.generator import generate_diff
 
 file1 = 'tests/fixtures/file1.yml'
 file2 = 'tests/fixtures/file2.yml'
+file1_txt = 'tests/fixtures/file1.txt'
+file2_txt = 'tests/fixtures/file2.txt'
+file1_2_txt = 'tests/fixtures/result_f1_f2_json.txt'
+plain_txt = 'tests/fixtures/file2.txt'
+json_txt = 'tests/fixtures/file2.txt'
 
 
 def test_file1_vs_file2():
-    assert generate_diff(file1, file2) == open('tests/fixtures/result_f1_f2_json.txt').read().strip()
+    assert generate_diff(file1, file2) == open(file1_2_txt).read().strip()
 
 
 def test_file1_vs_file1():
-    assert generate_diff(file1, file1) == open('tests/fixtures/file1.txt').read().strip()
+    assert generate_diff(file1, file1) == open(file1_txt).read().strip()
 
 
 def test_file2_vs_file2():
-    assert generate_diff(file2, file2) == open('tests/fixtures/file2.txt').read().strip()
+    assert generate_diff(file2, file2) == open(file2_txt).read().strip()
+
 
 def test_plain():
-    assert generate_diff(file1, file2, 'plain') == open('tests/fixtures/result_plain.txt').read().strip()
+    assert generate_diff(file1, file2, 'plain') == open(plain_txt).read().strip()
 
 
 def test_json():
-    assert generate_diff(file1, file2, 'json') == open('tests/fixtures/result_json.txt').read().strip()
+    assert generate_diff(file1, file2, 'json') == open(json_txt).read().strip()
